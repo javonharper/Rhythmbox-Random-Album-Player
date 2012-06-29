562b4944b72d770d8a6cbc11e4ad4a1551cb4aaa
Javon Harper <javon.d.harper@gmail.com>
2012-06-28 22:28:26 -0400

    Add random album item to menu

--- Rhythmbox-Random-Album-Player.py
+++ Rhythmbox-Random-Album-Player.py
@@ -20,6 +20,19 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 from gi.repository import GObject
 from gi.repository import Peas
 from gi.repository import RB
+from gi.repository import Gtk
+
+#finding a file
+# rb_find_plugin_data_file("myfile")
+random_album_menu_item = '''
+  <ui>
+    <menubar name="MenuBar">
+      <menu name="ControlMenu" action="Control">
+        <menuitem name="RandomAlbumItem" action="RandomAlbum"/>
+      </menu>
+    </menubar>
+  </ui>
+'''
 
 class RandomAlbumPlugin(GObject.Object, Peas.Activatable):
   object = GObject.property(type=GObject.Object)
@@ -28,4 +41,18 @@ class RandomAlbumPlugin(GObject.Object, Peas.Activatable):
     super(RandomAlbumPlugin, self).__init__()
 
   def do_activate(self):
-    print "Hello World!"
+    print "Activating Random Album Plugin"
+    shell = self.object
+    action = Gtk.Action ('RandomAlbum', _('Random Album'), _('Play a Random Album'), "")
+    action.connect ('activate', self.random_album, shell)
+    action_group = Gtk.ActionGroup('RandomAlbumActionGroup')
+    action_group.add_action(action)
+    ui_manager = shell.props.ui_manager
+    ui_manager.insert_action_group(action_group)
+    ui_manager.add_ui_from_string(random_album_menu_item)
+
+  def do_deactivate(self):
+    print 'Deactivating Random Album Plugin'
+
+  def random_album(self, event, shell):
+    print 'Playing Random Album'
