d6ce02bcd9fc0740068d2d79c3fe3ab6da4f1a10
Javon Harper <javon.d.harper@gmail.com>
2013-01-05 20:11:16 -0500

    Save and retrieval for ignored albums and album count

--- RandomAlbumConfigDialog.py
+++ RandomAlbumConfigDialog.py
@@ -10,4 +10,23 @@ class ConfigDialog(GObject.Object, PeasGtk.Configurable):
     def do_create_configure_widget(self):
         self.ui = Gtk.Builder()
         self.ui.add_from_file(rb.find_plugin_file(self, DIALOG_UI_FILE))
+        
+        # Load the settings from Gio
+        self.settings = Gio.Settings('com.javonharper.rhythmbox.plugins.randomalbumplayer')
+        
+        self.albums_to_play = self.ui.get_object('countSpinButton')
+        self.albums_to_play.set_value(self.settings['albums-to-play'])
+        
+        self.ignored_albums = self.ui.get_object('ignoreEntry')
+        self.ignored_albums.set_text(self.settings['ignored-albums'])
+        
+        self.albums_to_play.connect('value-changed', self.on_albums_to_play_changed)
+        self.ignored_albums.connect('changed', self.on_ignored_albums_changed)
+        
         return self.ui.get_object('dialog')
+    
+    def on_albums_to_play_changed(self, widget):
+        self.settings['albums-to-play'] = self.albums_to_play.get_value();
+    
+    def on_ignored_albums_changed(self, widget):
+        self.settings['ignored-albums'] = self.ignored_albums.get_text();
